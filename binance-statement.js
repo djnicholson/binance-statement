5c4236085eafe5db27e562213a234815d2452734
David Nicholson <me@djnicholson.com>
2019-04-25 16:35:03 +0100

    program options

---
+++ binance-statement.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const colors = require('colors');
+const program = require('commander');
+
+const makeStatement = require('./make-statement');
+
+program
+    .version('1.0.0', '-v, --version')
+    .description('Creates a financial statement for a Binance account')
+    .option('-k, --api-key <key>', 'Binance API key')
+    .option('-s, --api-secret <secret>', 'Binance API secret')
+    .option('-o, --output-file <file>', 'Output HTML file')
+    .option('-d, --data-file [file]', 'Data file')
+    .action(function(options) {
+        if (!options.apiKey) {
+            program.outputHelp();
+            console.error(colors.red('\nThe --api-key option is required\n'));
+        } else if (!options.apiSecret) {
+            program.outputHelp();
+            console.error(colors.red('\nThe --api-secret option is required\n'));
+        } else if (!options.outputFile) {
+            program.outputHelp();
+            console.error(colors.red('\nThe --output-file option is required\n'));
+        } else {
+            makeStatement(
+                options.apiKey,
+                options.apiSecret,
+                options.outputFile,
+                options.dataFile || (options.apiKey + ".db"));
+        }
+    });
+
+program.parse(process.argv);
---
+++ make-statement.js
@@ -0,0 +1,5 @@
+const main = (apiKey, apiSecret, outputFile, dataFile) => {
+    console.log("Making statement...", apiKey, apiSecret, outputFile, dataFile);
+};
+
+module.exports = main;

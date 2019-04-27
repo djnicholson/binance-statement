df44eb2203a668a727eb922d7b9e178d017a0963
David Nicholson <me@djnicholson.com>
2019-04-27 16:16:01 +0100

    client code placeholder

--- html-writer.js
+++ html-writer.js
@@ -19,22 +19,27 @@ class HtmlWriter {
     }
 
     async begin() {
+
         const templateFile = path.join(__dirname, 'template.html');
         const templateContents = await readFileContents(templateFile);
         const templateParts = templateContents.split('<!--inject(events)-->', 2);
         const preludeTemplate = templateParts[0];
         this.epilogue = templateParts[1];
 
+        const rendererFile = path.join(__dirname, 'renderer.js');
+        const rendererCode = await readFileContents(rendererFile);
+
         const bootstrapCss = await getDependencySource('bootstrap/dist/css/bootstrap.min.css');
         const jqueryJs = await getDependencySource('jquery/dist/jquery.min.js');
-        const popperJs = await getDependencySource('popper.js/dist/popper.min.js');
+        const popperJs = await getDependencySource('popper.js/dist/umd/popper.min.js');
         const bootstrapJs = await getDependencySource('bootstrap/dist/js/bootstrap.min.js');
 
         const prelude = preludeTemplate
             .replace('<!--inject(bootstrapCSS)-->', '<style type="text/css">' + bootstrapCss + '</style>')
             .replace('<!--inject(jqueryJs)-->', '<script>' + jqueryJs + '</script>')
             .replace('<!--inject(popperJs)-->', '<script>' + popperJs + '</script>')
-            .replace('<!--inject(bootstrapJs)-->', '<script>' + bootstrapJs + '</script>');
+            .replace('<!--inject(bootstrapJs)-->', '<script>' + bootstrapJs + '</script>')
+            .replace('<!--inject(rendererCode)-->', '<script>' + rendererCode + '</script>');
 
         this.file.write(prelude);
 
@@ -46,6 +51,7 @@ class HtmlWriter {
     }
 
     end() {
+        this.file.write('statement.loadingComplete();\r\n');
         this.file.write('</script>');
         this.epilogue && this.file.write(this.epilogue);
         this.file.end();
---
+++ renderer.js
@@ -0,0 +1,11 @@
+var statement = new(function() {
+
+    this.pushEvent = function(unitOfAccount, event) {
+
+    };
+
+    this.loadingComplete = function() {
+
+    };
+
+})();
